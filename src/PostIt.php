78de6306041d0154d80fc4af2e46d7d4da219075
Vampyrian <17021540+Vampyrian@users.noreply.github.com>
2025-02-20 18:46:44 +0000

    Fix styling

--- src/PostIt.php
+++ src/PostIt.php
@@ -36,186 +36,217 @@ class PostIt
     public function wherePostCode(string $postCode): self
     {
         $this->queryParams['post_code'] = $postCode;
+
         return $this;
     }
 
     public function wherePost(string $post): self
     {
         $this->queryParams['post'] = $post;
+
         return $this;
     }
 
     public function whereMunicipality(string $municipality): self
     {
         $this->queryParams['municipality'] = $municipality;
+
         return $this;
     }
 
     public function whereCity(string $city): self
     {
         $this->queryParams['city'] = $city;
+
         return $this;
     }
 
     public function whereAddress(string $address): self
     {
         $this->queryParams['address'] = $address;
+
         return $this;
     }
 
     public function whereStreet(string $street): self
     {
         $this->queryParams['street'] = $street;
+
         return $this;
     }
 
     public function whereNumber(string $number): self
     {
         $this->queryParams['number'] = $number;
+
         return $this;
     }
 
     public function limit(int $limit): self
     {
         $this->queryParams['limit'] = $limit;
+
         return $this;
     }
 
     public function page(int $page): self
     {
         $this->queryParams['page'] = $page;
+
         return $this;
     }
 
     public function wideNumberOn(): self
     {
         $this->queryParams['wide_number'] = 1;
+
         return $this;
     }
 
     public function wideNumberOff(): self
     {
         $this->queryParams['wide_number'] = 0;
+
         return $this;
     }
 
     public function groupByMunicipality(): self
     {
         $this->queryParams['group'] = 'municipality';
+
         return $this;
     }
 
     public function groupByMunicipalityCity(): self
     {
         $this->queryParams['group'] = 'municipality_city';
+
         return $this;
     }
 
     public function groupByCity(): self
     {
         $this->queryParams['group'] = 'city';
+
         return $this;
     }
 
     public function groupByStreet(): self
     {
         $this->queryParams['group'] = 'street';
+
         return $this;
     }
 
     public function orderByPostCode(): self
     {
         $this->order['post_code'] = Order::ASC;
+
         return $this;
     }
 
     public function orderByPostCodeDesc(): self
     {
         $this->order['post_code'] = Order::DESC;
+
         return $this;
     }
 
     public function orderByCity(): self
     {
         $this->order['city'] = Order::ASC;
+
         return $this;
     }
 
     public function orderByCityDesc(): self
     {
         $this->order['city'] = Order::DESC;
+
         return $this;
     }
 
     public function orderByAddress(): self
     {
         $this->order['address'] = Order::ASC;
+
         return $this;
     }
 
     public function orderByAddressDesc(): self
     {
         $this->order['address'] = Order::DESC;
+
         return $this;
     }
 
     public function orderByStreet(): self
     {
         $this->order['street'] = Order::ASC;
+
         return $this;
     }
 
     public function orderByStreetDesc(): self
     {
         $this->order['street'] = Order::DESC;
+
         return $this;
     }
 
     public function orderByNumber(): self
     {
         $this->order['number'] = Order::ASC;
+
         return $this;
     }
 
     public function orderByNumberDesc(): self
     {
         $this->order['number'] = Order::DESC;
+
         return $this;
     }
 
     public function orderByMunicipality(): self
     {
         $this->order['municipality'] = Order::ASC;
+
         return $this;
     }
 
     public function orderByMunicipalityDesc(): self
     {
         $this->order['municipality'] = Order::DESC;
+
         return $this;
     }
 
     public function orderByMunicipalitySize(): self
     {
         $this->order['municipality_size'] = Order::ASC;
+
         return $this;
     }
 
     public function orderByMunicipalitySizeDesc(): self
     {
         $this->order['municipality_size'] = Order::DESC;
+
         return $this;
     }
 
     public function orderByPost(): self
     {
         $this->order['post'] = Order::ASC;
+
         return $this;
     }
 
     public function orderByPostDesc(): self
     {
         $this->order['post'] = Order::DESC;
+
         return $this;
     }
 
@@ -247,6 +278,7 @@ class PostIt
         foreach ($this->order as $key => $order) {
             $url = $url . '&' . 'order=' . $key . '-' . $order->value;
         }
+
         return $url;
     }
 
